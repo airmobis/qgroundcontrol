4bf9c20aed1c63ae8af59022772a6c41dba19db8
davidsastresas <davidsastresas@gmail.com>
2024-05-19 12:33:04 +0200

    VideoSettings: Add settings for Herelink HDMI source

--- src/Settings/VideoSettings.h
+++ src/Settings/VideoSettings.h
@@ -35,6 +35,8 @@ public:
     DEFINE_SETTINGFACT(disableWhenDisarmed)
     DEFINE_SETTINGFACT(lowLatencyMode)
     DEFINE_SETTINGFACT(forceVideoDecoder)
+    // Herelink HDMI source switching
+    DEFINE_SETTINGFACT(cameraId)
 
     Q_PROPERTY(bool     streamConfigured        READ streamConfigured       NOTIFY streamConfiguredChanged)
     Q_PROPERTY(QString  rtspVideoSource         READ rtspVideoSource        CONSTANT)
