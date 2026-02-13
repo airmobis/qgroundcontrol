1919f44d8538ef5c256a2c1c7ee34fcca5b8cb4b
Petr Šácha <petrik.sacha@gmail.com>
2026-02-13 11:56:44 +0100

    re-implement Qt version check for deprecation prevention

--- src/QmlControls/QGCImageProvider.h
+++ src/QmlControls/QGCImageProvider.h
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include <QtCore/qnamespace.h>
 #include <QtCore/QObject>
 #include <QtGui/QImage>
 #include <QtQuick/QQuickImageProvider>
@@ -21,7 +22,13 @@ public:
     ~QGCImageProvider();
 
     QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) final;
-    void setImage(const QImage &image, uint8_t vehicleId = 0) { _images[vehicleId] = image.mirrored(); }
+    void setImage(const QImage &image, uint8_t vehicleId = 0) {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+        _images[vehicleId] = image.flipped(Qt::Horizontal);
+#else
+        _images[vehicleId] = image.mirrored();
+#endif
+    }
 
 private:
     QMap<uint8_t, QImage> _images;
